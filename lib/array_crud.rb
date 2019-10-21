fb077d80a0f66cc24362c15dbaabf375aa68ae54
saman batool <samanbatool08@gmail.com>
2019-10-21 18:57:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,7 @@ def add_element_to_end_of_array(array, element)
   different_colors = ["blue","green","purple","red"]
   different_colors << "orange"
 end
+p different_colors
 
 def add_element_to_start_of_array(array, element)
   
