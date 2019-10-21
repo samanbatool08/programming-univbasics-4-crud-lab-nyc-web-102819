2f24194739b13cdf3a4f5ac48f26fdcb694c83ed
saman batool <samanbatool08@gmail.com>
2019-10-21 19:03:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,13 +32,13 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  different_colors[ 0 ]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  different_colors[ -1 ]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  different_colors[ 1 ] = "gray"
 end
